4e788f1d98aa43b2a787696841c106ccbd84b57f
Emiliano Macías <emi.llanas@hotmail.com>
2021-05-06 22:41:31 -0500

    Conexion con firebase

--- app/src/main/java/mx/aepc/proyectoparcial1moviles/MainActivity.java
+++ app/src/main/java/mx/aepc/proyectoparcial1moviles/MainActivity.java
@@ -6,12 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Mawi");
+        user.put("last", "Selacome2");
+        user.put("born", 2000);
+        db.collection("users").document("123@gmail.com").set(user);
+
         //comentario de prueba de mawi a ver que pdo 2:11 pm 6 mayo 2021
     }
 
