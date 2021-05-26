ebf52762b226d4457c6452a32a8cde1fcbdd2bb2
AmauriPC <mawi.x48@hotmail.com>
2021-05-26 00:28:48 -0500

    logo

--- app/src/main/java/mx/aepc/proyectoparcial1moviles/MainActivity.java
+++ app/src/main/java/mx/aepc/proyectoparcial1moviles/MainActivity.java
@@ -2,11 +2,18 @@ package mx.aepc.proyectoparcial1moviles;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +23,9 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements OnCompleteListener<QuerySnapshot> {
 
@@ -28,7 +38,37 @@ public class MainActivity extends AppCompatActivity implements OnCompleteListene
         setContentView(R.layout.activity_main);
         cont=(TextView)findViewById(R.id.textContraseña);
         username=(TextView)findViewById(R.id.textEmail);
+
+        if(isStoragePermissionGranted())
+        {
+            Log.println(Log.ASSERT,"OK","Permisos concedidos");
+        }
+
     }
+
+
+    public boolean isStoragePermissionGranted() {
+        final int STORAGE_PERMISSION = 100;
+        int ACCESS_EXTERNAL_STORAGE = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if ((ACCESS_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+            return false;
+        }
+        return true;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        final int STORAGE_PERMISSION = 100;
+        if (requestCode == STORAGE_PERMISSION && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.println(Log.ASSERT,"OK","Permisos obtenidos");
+        }
+        else
+        {
+            Log.println(Log.ASSERT,"NOK","No pos fue GG ya ni que hacerle");
+        }
+    }
+
 
     public void getActivityDatos(View view)
     {
@@ -46,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements OnCompleteListene
     @Override
     public void onComplete(@NonNull Task<QuerySnapshot> task) {
         Log.i("test","testeando");
-        if (task.isSuccessful()) {
+        if (false) {
             for (QueryDocumentSnapshot document : task.getResult()) {
                 if(document.getId().equals(username.getText().toString())) {
                     Intent intent = new Intent(this, pantallaIntereses.class);
@@ -55,7 +95,13 @@ public class MainActivity extends AppCompatActivity implements OnCompleteListene
                 }
             }
         } else {
-            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            Log.println(Log.ASSERT,"Nel","Contraseña o usuario incorrecto");
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(getBaseContext(), "Usuario o contraseña incorrectos", duration);
+            toast.show();
+
+            //Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
         }
+        //Log.println(Log.ASSERT,"prueba","prueba"+task.isSuccessful());
     }
 }
